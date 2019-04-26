6f731b762eeb69b39ccfda8a766837b61cb896df
Sandeepsingh Multani <Sandeepsingh.Multani@altiusva.com>
2019-04-26 09:19:36 +0100

    created a receiver and sender

---
+++ RabbitMQReceiver/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MassTransit;
+using RabbitMQCommon;
+
+namespace RabbitMQReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                sbc.ReceiveEndpoint(host, "guest_queue", ep =>
+                {
+                    ep.Handler<Message>(context => Console.Out.WriteLineAsync($"Received from {context.Message.Sender}[{context.Message.Id}]: {context.Message.Text}"));
+                });
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Receiver Started.");
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
